869ca66a248ad2c560dcee64c5f4cbf9c92def5a
Demitri-Ansss <ansoricahgreges@gmail.com>
2026-02-13 16:11:25 +0700

    UPDATE INDEX.PHP DAN GITIGNORE

--- api/index.php
+++ api/index.php
@@ -8,6 +8,14 @@
 | a helper to run migrations in production if needed.
 */
 
+// Map cache to /tmp for Vercel (Read-only filesystem)
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_PATH=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 try {
     if (getenv('RUN_MIGRATIONS_ON_DEPLOY') === 'true') {
         require __DIR__ . '/../vendor/autoload.php';
